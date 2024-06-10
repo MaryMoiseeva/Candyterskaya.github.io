cd3c07efbc65b51de4166eff06b63b32b68f6eb3
MaryMoiseeva <161971435+MaryMoiseeva@users.noreply.github.com>
2024-06-10 21:09:34 +0400

    script1.js

--- script1.js
+++ script1.js
@@ -1,25 +1,14 @@
-const login = document.getElementById('login');
+login.addEventListener('submit', function(event) {
+    // Предотвращаем стандартное поведение отправки формы
+    event.preventDefault();
 
-// Добавляем обработчик события onSubmit
-feedback.addEventListener('submit', function(event) {
-    event.preventDefault(); // Отменяем переход на другую страницу при отправке формы
-
-    // Получаем значения из полей формы
+    // Получаем значения полей формы
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const role = document.getElementById('role').value;
 
-    // Отображаем данные из формы в консоли
+    // Выводим значения в консоль
     console.log('Имя пользователя:', username);
     console.log('Пароль:', password);
     console.log('Роль:', role);
-
-    // Добавляем сообщение об успешной обработке данных (можно использовать библиотеку jQuery Modal или Bootstrap Modal Dialog)
-    alert('Вы зашли под ролью:');
-
-    // Очищаем поля формы
-    document.getElementById('username').value = '';
-    document.getElementById('password').value = '';
-    document.getElementById('role').value = '';
 });
-
